9ce7cf861488ebd902a79d1720eaefaa7db088ab
Robert Rascon <rrascon22@gmail.com>
2018-04-26 10:34:25 -0700

    Revert "Carbonify Again"
    This reverts commit 8a29ededb6e7630f82b7ca01610961d351ff6a9f.

--- lineage.mk
+++ lineage.mk
@@ -1,16 +1,16 @@
 $(call inherit-product, device/lge/h918/full_h918.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/carbon/config/common.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Overlays (inherit after vendor/cm to ensure we override it)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-PRODUCT_NAME := carbon_h918
+PRODUCT_NAME := lineage_h918
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="v20" \
-    PRODUCT_NAME="carbon_elsa_tmo_us" \
+    PRODUCT_NAME="elsa_tmo_us" \
     PRIVATE_BUILD_DESC="elsa_tmo_us-user 7.0 NRD90M 1625821141c30 release-keys"
 
 BUILD_FINGERPRINT := "lge/elsa_tmo_us/elsa:7.0/NRD90M/1625821141c30:user/release-keys"
